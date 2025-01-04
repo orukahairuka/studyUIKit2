8cc304a4d9bb8e64e07969a3d390f03b976cb245
irukahaoruka <erika03301109@gmail.com>
2025-01-04 20:26:28 +0900

    a

--- studyUIKit/ReminderListViewController.swift
+++ studyUIKit/ReminderListViewController.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //リストの見た目を設定
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
-        
+
+        //セルのデザインと内容を設定
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            //サンプルデータの取得
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            //せるの内容を設定。セルのテキスト部分にリマインダーのtitleを設定する
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
+
+        //データソースを作成
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
     }
-    
+
+    //リストの外観を設定している
     private func listLayout () -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.showsSeparators = false
