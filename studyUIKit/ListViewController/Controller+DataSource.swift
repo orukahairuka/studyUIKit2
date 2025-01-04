b0b4af2bb02aa42d1dbd0dac0b8781dd77ed30c2
irukahaoruka <erika03301109@gmail.com>
2025-01-04 21:53:08 +0900

    a

--- studyUIKit/ListViewController/Controller+DataSource.swift
+++ studyUIKit/ListViewController/Controller+DataSource.swift
@@ -14,11 +14,44 @@ extension ReminderListViewController {
 
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
         let reminder = Reminder.sampleData[indexPath.item]
+
+        //セルにタイトルとサブテキストの設定
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(
             forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
+
+        //カスタムボタンの設定
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        //カスタムビューと矢印を設定
+        cell.accessories = [
+            .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)
+        ]
+
+        //背景色の設定
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
     }
+
+    //リマインダーが完了ずみか未完了かを表すボタン
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        //ボタンをセルの左側に配置し、常に表示されるように設定する
+        return UICellAccessory.CustomViewConfiguration (
+            customView: button, placement: .leading(displayed: .always)
+        )
+    }
+}
+
+extension UIColor {
+    static let todayListCellDoneButtonTint = UIColor.systemBlue // ボタンの色を設定
+    static let todayListCellBackground = UIColor.systemGray6 // 背景色を設定
 }
